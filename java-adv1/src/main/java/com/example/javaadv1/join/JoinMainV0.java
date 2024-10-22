f47444eab1d3a3ef1c6b27ab356a531375830a62
Hosung <an3735297@pusan.ac.kr>
2024-10-22 18:27:04 +0900

    Thread - 직렬 실행, 병렬 실행

--- java-adv1/src/main/java/com/example/javaadv1/join/JoinMainV0.java
+++ java-adv1/src/main/java/com/example/javaadv1/join/JoinMainV0.java
@@ -7,15 +7,37 @@ public class JoinMainV0 {
     public static void main(String[] args) throws InterruptedException {
         MyLogger.log("main() start");
 
+        Thread thread1 = new Thread(new Task());
+        Thread thread2 = new Thread(new Task());
+        Thread thread3 = new Thread(new Task());
+        // 직렬 실행 -> 약 9초 소모
+//         thread1.start();
+//         thread1.join();
+//         thread2.start();
+//         thread2.join();
+//         thread3.start();
+//         thread3.join();
 
-        Job task = new Job(1, 50);
-        Thread thread1 = new Thread(task);
+        // 병렬 실행 -> 약 3초 소모
         thread1.start();
-        thread1.join();
+        thread2.start();
+        thread3.start();
 
-        MyLogger.log("result : " + task.result);
+        thread1.join();
+        thread2.join();
+        thread3.join();
 
         MyLogger.log("main() end");
+    }
+
+    static class Task implements Runnable {
+        @Override
+        public void run() {
+            for (int i=1;i<=3;i++) {
+                MyLogger.log("Task: " + i);
+                ThreadUtils.sleep(1000);
+            }
+        }
     }
 
     static class Job implements Runnable {
